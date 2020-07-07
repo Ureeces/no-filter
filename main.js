c485e2838f4c1f98d6c5d7b42afe14fb09c56c0e
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-07 12:12:33 -0400

    Finished all functions.

--- main.js
+++ main.js
@@ -1,7 +1,56 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+ const onlyOdds = function(numbers) {
+  const odds = [];
 
+  for(let num of numbers) {
+    const isOdd = num % 2 !== 0 ? true : false;
+    if(isOdd) {
+      odds.push(num);
+    }
+  }
+
+  return odds;
+}
+
+const onlyEvens = function(numbers) {
+  const evens = [];
+
+  for(let num of numbers) {
+    const isEven = num % 2 === 0 ? true : false;
+    if(isEven) {
+      evens.push(num);
+    }
+  }
+
+  return evens;
+}
+
+const shortNamesOnly = function(names) {
+  const shortNames = [];
+
+  for(let name of names) {
+    if(name.length < 7) {
+      shortNames.push(name);
+    }
+  }
+
+  return shortNames;
+}
+
+const dNames = function(names) {
+  const dNS = [];
+
+  for(let name of names) {
+    let firstLetter = name[0].toUpperCase();
+    if(firstLetter === "D") {
+      dNS.push(name);
+    }
+  }
+
+  return dNS;
+}
 
 
 
